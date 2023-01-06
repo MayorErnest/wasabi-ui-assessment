f6174839450127bdf52508a4454a3d0588777299
Obumma Ernest <eobumma@gmail.com>
2023-01-06 14:37:54 +0100

    --feat: complete header

--- assets/svgs/index.ts
+++ assets/svgs/index.ts
@@ -2,5 +2,7 @@ import Logo from "./logo.svg";
 import Avatar from "./avatar.svg";
 import ArrowDown from "./arrowDown.svg";
 import SearchIcon from "./searchIcon.svg";
+import HamburgerIcon from "./hamburger.svg";
+import CloseIcon from "./close.svg";
 
-export { Logo, Avatar, ArrowDown, SearchIcon };
+export { Logo, Avatar, ArrowDown, SearchIcon, HamburgerIcon, CloseIcon };
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { css } from "styled-components";
-import { ArrowDown, Avatar, Logo } from "../../assets";
+import {
+	ArrowDown,
+	Avatar,
+	CloseIcon,
+	HamburgerIcon,
+	Logo,
+} from "../../assets";
 import { Search } from "../search";
 
-type StyledHeaderDivProp = {
+type StyledFlexProp = {
 	isFlex?: boolean;
 };
 
+type StyledResponsiveProp = {
+	isMobile?: boolean;
+};
+
+type StyledSmallNavProp = {
+	isOpen?: boolean;
+};
+
 export const Header = () => {
+	const [showSmallNav, setShowSmallNav] = useState<boolean>(false);
 	return (
 		<StyledHeader>
+			{/* Big header */}
 			<StyledHeaderContainer>
 				<StyledHeaderDiv isFlex>
 					<Logo />
@@ -29,6 +45,21 @@ export const Header = () => {
 					<ArrowDown />
 				</StyledSearchAndMenuContainer>
 			</StyledHeaderContainer>
+			{/* Small header */}
+			<StyledHeaderContainer isMobile>
+				<Logo />
+				<HamburgerIcon onClick={() => setShowSmallNav(true)} />
+				<StyledSmallNav isOpen={showSmallNav}>
+					<StyledListContainer isMobile>
+						<CloseIcon onClick={() => setShowSmallNav(false)} />
+						<StyledListItem isMobile>Home</StyledListItem>
+						<StyledListItem isMobile>About</StyledListItem>
+						<StyledListItem isMobile>Solutions</StyledListItem>
+						<StyledListItem isMobile>Pricing</StyledListItem>
+						<StyledListItem isMobile>Contact</StyledListItem>
+					</StyledListContainer>
+				</StyledSmallNav>
+			</StyledHeaderContainer>
 		</StyledHeader>
 	);
 };
@@ -37,22 +68,51 @@ const StyledHeader = styled.header`
 	background: ${(props) => props.theme["--wasabi-white"]};
 `;
 
-const StyledHeaderContainer = styled.div`
-	display: none;
-	align-items: center;
-	justify-content: space-between;
-	max-width: ${(props) => props.theme["--wasabi-max-width"]};
-	margin: 0 auto;
-	@media (min-width: 992px) {
-		display: flex;
-		padding: 0rem 4%;
-	}
-	@media (min-width: 1280px) {
-		padding: 0rem 0%;
-	}
+const StyledHeaderContainer = styled.div<StyledResponsiveProp>`
+	${(props) => {
+		switch (props.isMobile) {
+			case true:
+				return css`
+					display: flex;
+					align-items: center;
+					justify-content: space-between;
+					max-width: ${(props) => props.theme["--wasabi-max-width"]};
+					margin: 0 auto;
+					padding: 2rem 4%;
+					& > div > svg:first-of-type {
+						width: 9.5rem;
+					}
+					& > div > svg:first-of-type > * {
+						transform: scale(0.8);
+					}
+					@media (min-width: 992px) {
+						display: none;
+						padding: 0rem 4%;
+					}
+					@media (min-width: 1280px) {
+						padding: 0rem 0%;
+					}
+				`;
+			default:
+				return css`
+					display: none;
+					align-items: center;
+					justify-content: space-between;
+					max-width: ${(props) => props.theme["--wasabi-max-width"]};
+					margin: 0 auto;
+					@media (min-width: 992px) {
+						display: flex;
+						padding: 0rem 4%;
+					}
+					@media (min-width: 1280px) {
+						padding: 0rem 0%;
+					}
+				`;
+		}
+	}}
 `;
 
-const StyledHeaderDiv = styled.div<StyledHeaderDivProp>`
+const StyledHeaderDiv = styled.div<StyledFlexProp>`
 	${(props) => {
 		switch (props.isFlex) {
 			case true:
@@ -68,16 +128,17 @@ const StyledHeaderDiv = styled.div<StyledHeaderDivProp>`
 	}}
 `;
 
-const StyledListContainer = styled.ul`
+const StyledListContainer = styled.ul<StyledResponsiveProp>`
 	list-style-type: none;
 	display: flex;
+	flex-direction: ${(props) => (props.isMobile ? "column" : "row")};
 	margin-left: 2.5rem;
 	padding: 1.7rem 0rem;
 `;
 
-const StyledListItem = styled.li`
-	margin-right: 1rem;
-	font-size: 0.875rem;
+const StyledListItem = styled.li<StyledResponsiveProp>`
+	margin: ${(props) => (props.isMobile ? "1.5rem 0rem" : "0rem 1rem")};
+	font-size: ${(props) => (props.isMobile ? "1.5rem" : "0.875rem")};
 	color: ${(props) => props.theme["--wasabi-blue-100"]};
 	&:nth-of-type(1) {
 		color: ${(props) => props.theme["--wasabi-blue-50"]};
@@ -93,5 +154,19 @@ const StyledSearchAndMenuContainer = styled.div`
 	}
 	& svg:last-of-type {
 		margin-left: 0.3rem;
+	}
+`;
+
+const StyledSmallNav = styled.nav<StyledSmallNavProp>`
+	display: ${(props) => (props.isOpen ? "block" : "none")};
+	position: fixed;
+	inset: 0;
+	background: ${(props) => props.theme["--wasabi-white"]};
+	z-index: 10;
+	& svg {
+		position: absolute;
+		right: 1rem;
+		width: 50px;
+		height: 50px;
 	}
 `;
