f397c1a032fd0c73cff000c0f1afe8a0cfa22ae9
Obumma Ernest <eobumma@gmail.com>
2023-01-07 09:50:07 +0100

    --feat: add schools data

---
+++ assets/data.ts
@@ -0,0 +1,84 @@
+export const schoolsData = {
+	schools: {
+		school1: {
+			name: "Deal School District",
+			image: "https://www.collinsdictionary.com/images/full/school_309241295.jpg",
+		},
+		school2: {
+			name: "Salem City School, New Jersey",
+			image: `https://img.freepik.com/free-vector/school-building-educational-institution-college_107791-1051.jpg?w=2000`,
+		},
+	},
+	schoolsTableData: {
+		"FINDEET RATING": {
+			"STUDENT-TEACHER RATIO": {
+				school1: "89",
+				school2: "45",
+			},
+			"AVERAGE TEACHER SALARY": {
+				school1: "2:9",
+				school2: "",
+			},
+			"ONLINE TEACHING": {
+				school1: "34",
+				school2: "69",
+			},
+		},
+		"KEY SCHOOL STATS": {
+			"SCHOOL ID CARD": {
+				school1: true,
+				school2: true,
+			},
+			"LIBRARY DURATION": {
+				school1: 350,
+				school2: 300,
+			},
+			"RESUMPTION DATE": {
+				school1: "Soap",
+				school2: "Soap",
+			},
+		},
+		"ACADEMIC STATS": {
+			"SCHOOL FEES": {
+				school1: "yes",
+				school2: "Naa",
+			},
+			"LIBRARY STAT": {
+				school1: "233.9",
+				school2: "90",
+			},
+			"CLOSING DATE": {
+				school1: "March",
+				school2: "April",
+			},
+		},
+		"TEACHER STATS": {
+			"SCHOOL UNIFORM": {
+				school1: "yes",
+				school2: "Naa",
+			},
+			STAT: {
+				school1: "233.9",
+				school2: "90",
+			},
+			"RESUMPTION DATE": {
+				school1: "Soap",
+				school2: "Bag",
+			},
+		},
+		"FEE STRUCTURE": {
+			"SCHOOL BUS": {
+				school1: "100",
+				school2: "200",
+			},
+			"LIBRARY BOOKS": {
+				school1: "9",
+				school2: "90",
+			},
+			"BREAK TIME": {
+				school1: "2pm",
+				school2: "1pm",
+			},
+		},
+	},
+};
