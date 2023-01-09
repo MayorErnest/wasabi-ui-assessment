d95177306888aad2d5f2155bade2742abb23d787
Obumma Ernest <eobumma@gmail.com>
2023-01-09 16:20:47 +0100

    --cleanup: demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,8 @@
-import axios from "axios";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import styled from "styled-components";
 import { schoolsData } from "../assets/data";
 import { ComparisonTable, Header, Search } from "../components";
 
 export default function Home() {
-	const { query, push } = useRouter();
-
-	if (query) {
-		axios(`https://isdhidhidhsi?q=${query.token}`)
-			.then((res) => console.log(res))
-			.catch((err) => push("/dummy"));
-		return <></>;
-	}
-
 	return (
 		<>
 			<Header />
